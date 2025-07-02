97fb48b99eb7f0ae270952466b9465323c739983
shaktiprasad.mohanty <shaktiprasad.mohanty@quipserve.com>
2025-07-02 10:05:31 +0530

    fix: update API key in football API configuration

--- src/api/football.js
+++ src/api/football.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default axios.create({
   baseURL: 'https://v3.football.api-sports.io',
   headers: {
-    'x-rapidapi-key': '4fdfbd0168516e6feb9986eaf336b2c1',
+    'x-rapidapi-key': '781dc739cf9751c1a9f58ae192afb047',
     'x-rapidapi-host': 'v3.football.api-sports.io'
   }
 })
